a656c54f738f9c48d380ed290a71674fd5d71599
Júlia de Backer <55795526+debackerr@users.noreply.github.com>
2021-08-31 10:56:04 -0300

    Update common.cpp

--- common.cpp
+++ common.cpp
@@ -150,3 +150,36 @@ unsigned short getsDestiny(unsigned short exhibitorID){
     
 	return( (d == 0) ? d : exhibitorID);			
 }
+
+unsigned short returnsID( const vector<client> client, char c ){
+   if ( client.size() == 1){
+       switch (c)
+       {
+       case 'e':
+           return 4096; //first exhibitor's id
+           break;
+       case 'i':
+            return 1; //first issuer's id
+            break;
+       default:
+           return -1; //unrecognized type
+           break;
+       }
+   }else{
+       long unsigned int s = 1;
+       int aux = 0;
+       for( s = 1; s < client.size(); s++){
+           if( client[s].id - client[s-1].id > 1 ){
+            aux++; break;
+           }
+       }
+
+      if( aux == 0){
+        return ( client.back().id + 1);
+      }else{
+        return ( client[s-1].id + 1);
+      }
+       
+       
+   }
+}
